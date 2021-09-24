872ba140515b71f7509594337b45a52c11594c20
Igor Milione <47537077+imilione@users.noreply.github.com>
2021-09-23 23:08:27 -0300

    Nav dropdown, buttons w modals

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,26 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
-
+import { HttpClientModule } from '@angular/common/http';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
 
+import { EventoService } from './_services/evento.service';
+
+import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
-      EventosComponent,
-      NavComponent
-   ],
+    EventosComponent,
+    NavComponent,
+    DateTimeFormatPipePipe,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ModalModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [EventoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,50 +1,66 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Evento } from '../_models/Evento';
+import { EventoService } from '../_services/evento.service';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.css'],
 })
 export class EventosComponent implements OnInit {
-
-  _filtroLista: string = '';
-  get filtroLista(): string {
-    return this._filtroLista;
-  }
-  set filtroLista(value: string){
-    this._filtroLista = value;
-    this.eventosFiltrados = /*this.filtroLista ?*/ this.filtrarEventos(this.filtroLista) /*: this.eventos*/;
-  }
-  eventosFiltrados: any = [];
-  eventos: any = [];
+  eventosFiltrados: Evento[] = [];
+  eventos: Evento[] = [];
   imagemMargem = 2;
   imagemLargura = 30;
   mostrarImagem = true;
+  modalRef!: BsModalRef;
 
-  alternarImagem(){
+  _filtroLista: string = '';
+
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  alternarImagem() {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
-  filtrarEventos(filtrarPor: string) { 
-    filtrarPor = filtrarPor.toLocaleLowerCase() 
-    return this.eventos.filter((evento:any) => { 
-      return evento.tema.toLocaleLowerCase().includes(filtrarPor) 
-    }) 
+  filtrarEventos(filtrarPor: string) {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter((evento: any) => {
+      return evento.tema.toLocaleLowerCase().includes(filtrarPor);
+    });
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService
+  ) {}
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 
   ngOnInit() {
     this.getEventos();
   }
 
   getEventos() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
-      this.eventos = response;
-      this.eventosFiltrados = this.eventos;
-    }, error => {
-      console.log(error);
-    });
+    this.eventoService.getAllEvento().subscribe(
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
